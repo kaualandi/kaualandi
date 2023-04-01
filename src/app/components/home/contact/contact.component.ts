804f958abf0c0efd48b2fe3c24e2da7932e161a1
Kauã Landi <eu@kaualf.com>
2023-04-01 13:30:07 -0300

    feat: contact sent working

--- src/app/components/home/contact/contact.component.ts
+++ src/app/components/home/contact/contact.component.ts
@@ -1,3 +1,5 @@
+import { Contact } from './../../../models/contact';
+import { ContactService } from './../../../services/contact.service';
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
@@ -7,11 +9,45 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private contactService: ContactService
+  ) {}
+
+  loading = false;
+  statusSent: 'ok' | 'error' | null = null;
 
   contactForm = this.fb.group({
     name: ['', [Validators.required]],
     email: ['', [Validators.required, Validators.email]],
     message: ['', [Validators.required]],
   });
+
+  removeSentStatus() {
+    setTimeout(() => {
+      this.statusSent = null;
+    }, 3000);
+  }
+
+  handleSubmit() {
+    if (!this.contactForm.valid) return;
+
+    this.loading = true;
+
+    const form = this.contactForm.value as Contact;
+    this.contactService.sendForm(form).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.loading = false;
+        this.statusSent = 'ok';
+        this.removeSentStatus();
+      },
+      error: (error) => {
+        console.log(error);
+        this.loading = false;
+        this.statusSent = 'error';
+        this.removeSentStatus();
+      },
+    });
+  }
 }
