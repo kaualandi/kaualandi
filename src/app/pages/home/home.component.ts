f90f1cd2b0d78f282120d5de882db31e726569dc
Kauã Landi <eu@kaualf.com>
2023-11-19 00:29:58 -0300

    feat: removed game in tablet in responsive

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,12 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-  names = [
-    'João da Silva',
-    'Maria de Fátima',
-    'Carol do Santos',
-    'Lucas dos Santos',
-    'Ana e Luiz',
-  ];
-}
+export class HomeComponent {}
