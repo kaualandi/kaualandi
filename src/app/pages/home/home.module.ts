dbb5f9321b59809fd69355c916dac363baeb9f72
Kauã Landi <eu@kaualf.com>
2023-11-19 10:14:34 -0300

    refactor: removed unutil modules in home

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,22 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { SharedModule } from 'src/app/components/shared/shared.module';
-import { HomeComponent } from './home.component';
-import { HomeRoutingModule } from './home-routing.module';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { ZoomableDirective } from 'src/app/directives/zoomable.directive';
 import { SnakeGameComponent } from '../../components/home/snake-game/snake-game.component';
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    SharedModule,
-    HomeRoutingModule,
-    ReactiveFormsModule,
-    MatAutocompleteModule,
-  ],
+  imports: [CommonModule, SharedModule, HomeRoutingModule],
   declarations: [HomeComponent, ZoomableDirective, SnakeGameComponent],
 })
 export class HomeModule {}
