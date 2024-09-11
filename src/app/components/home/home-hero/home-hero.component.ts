7a7f80d03f2ca6fc5cae7b39f70ea3c9627ba4ea
Kauã Landi <eu@kaualf.com>
2024-09-11 16:03:13 -0300

    feat: add router for contacts home-hero component

--- src/app/components/home/home-hero/home-hero.component.ts
+++ src/app/components/home/home-hero/home-hero.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home-hero',
   standalone: true,
-  imports: [TranslateModule, MatButtonModule],
+  imports: [TranslateModule, MatButtonModule, RouterModule],
   templateUrl: './home-hero.component.html',
   styleUrl: './home-hero.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
