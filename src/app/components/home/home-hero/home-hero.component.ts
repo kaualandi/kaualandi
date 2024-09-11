449e457ee76bd843b3aa15d99a2da1ec6062b115
Kauã Landi <eu@kaualf.com>
2024-09-11 17:00:44 -0300

    feat: add scroll to bottom animation in home hero section

--- src/app/components/home/home-hero/home-hero.component.ts
+++ src/app/components/home/home-hero/home-hero.component.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { IconDirective } from '@app/directives/icon.directive';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home-hero',
   standalone: true,
-  imports: [TranslateModule, MatButtonModule, RouterModule],
+  imports: [TranslateModule, MatButtonModule, RouterModule, IconDirective],
   templateUrl: './home-hero.component.html',
   styleUrl: './home-hero.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
