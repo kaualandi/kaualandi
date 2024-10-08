e2543f0268d271241df3ad5c8f9dc9931c9cb8ea
Kauã Landi <eu@kaualf.com>
2024-10-08 13:50:33 -0300

    feat: translate skills section in home page

--- src/app/components/home/home-skills/home-skills.component.ts
+++ src/app/components/home/home-skills/home-skills.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home-skills',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './home-skills.component.html',
   styleUrl: './home-skills.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
